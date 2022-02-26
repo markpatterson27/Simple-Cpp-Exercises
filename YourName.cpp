91a72ba8c583cc552a7e3ca1759bd2762cbe38f3
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2022-02-26 09:38:09 +0000

    add YourName program answer

--- YourName.cpp
+++ YourName.cpp
@@ -2,8 +2,10 @@
 #include <string>
 
 int main() {
-    // Create your 'Your Name' program here.
-    // Your program should ask the user for their name and then output
-    // "Your name is " followed by the user's name.
-    
+    std::string name;
+
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+
+    std::cout << "Your name is " << name <<  std::endl;
 }
