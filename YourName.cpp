64d1ccb4992a3d43dfc5f883d19e1122f1c8cc21
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2022-02-26 09:38:09 +0000

    add YourName file and test

---
+++ YourName.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Create your 'Your Name' program here.
+    // Your program should ask the user for their name and then output
+    // "Your name is " followed by the user's name.
+    
+}
