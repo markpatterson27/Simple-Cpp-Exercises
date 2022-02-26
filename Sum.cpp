d54d787d955a8adb67853c3e955e3ea5491195c3
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2022-02-26 09:46:02 +0000

    add Sum file and test

---
+++ Sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // Create your Sum program here.
+    // Your program should ask the user to enter 2 integer numbers, and then
+    // output the sum of those 2 numbers.
+    
+}
